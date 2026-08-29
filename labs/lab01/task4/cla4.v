bb1ce1144f8ecd744a9b4679159f646d72df036b
SHASHWAT RAWAT <f20250515@pilani.bits-pilani.ac.in>
2026-08-29 11:05:17 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -21,53 +21,20 @@
 // TODO -- Step 3: sum bits
 //   sum[i] = p[i] ^ c[i]     (c0 = cin)
 
-module cla4(
-  input  [3:0] a,
-  input  [3:0] b,
-  input        cin,
-  output [3:0] sum,
-  output       cout
+module cla4 (
+    input  [3:0] a,
+    input  [3:0] b,
+    input        cin,
+    output [3:0] sum,
+    output       cout
 );
+    wire [3:0] g = a & b;
+    wire [3:0] p = a ^ b;
 
-  wire p0, p1, p2, p3;
-  wire g0, g1, g2, g3;
-  wire c1, c2, c3;
-  xor #(2) (p0,a[0],b[0]);
-  xor #(2) (p1,a[1],b[1]);
-  xor #(2) (p2,a[2],b[2]);
-  xor #(2) (p3,a[3],b[3]);
-
-  and #(2) (g0,a[0],b[0]);
-  and #(2) (g1,a[1],b[1]);
-  and #(2) (g2,a[2],b[2]);
-  and #(2) (g3,a[3],b[3]);
-
-  wire t1, t2;
-  wire t3, t4, t5;
-  wire t6, t7, t8, t9;
-  wire t10;
-
-  and #(2) (t1, p0, cin);
-  or  #(2) (c1, g0, t1);
-
-  and #(2) (t2, p1, g0);
-  and #(2) (t3, p1, p0, cin);
-  or  #(2) (c2, g1, t2, t3);
-
-  and #(2) (t4, p2, g1);
-  and #(2) (t5, p2, p1, g0);
-  and #(2) (t6, p2, p1, p0, cin);
-  or  #(2) (c3, g2, t4, t5, t6);
-
-  and #(2) (t7,  p3, g2);
-  and #(2) (t8,  p3, p2, g1);
-  and #(2) (t9,  p3, p2, p1, g0);
-  and #(2) (t10, p3, p2, p1, p0, cin);
-  or  #(2) (cout, g3, t7, t8, t9, t10);
-
-  xor #(2) (sum[0], p0, cin);
-  xor #(2) (sum[1], p1, c1);
-  xor #(2) (sum[2], p2, c2);
-  xor #(2) (sum[3], p3, c3);
-
+    wire c1 = g[0] | (p[0] & cin);
+    wire c2 = g[1] | (p[1] & g[0]) | (p[1] & p[0] & cin);
+    wire c3 = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & cin);
+    
+    assign cout = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & cin);
+    assign sum  = p ^ {c3, c2, c1, cin};
 endmodule
