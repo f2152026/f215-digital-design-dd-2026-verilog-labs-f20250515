376ac32525016fc42a5c40ba973c6aca2eb42293
SHASHWAT RAWAT <f20250515@pilani.bits-pilani.ac.in>
2026-08-29 10:32:37 +0000

    lab01 task4

--- labs/lab01/task4/cla64_blocked.v
+++ labs/lab01/task4/cla64_blocked.v
@@ -52,4 +52,4 @@ module cla64_blocked(
   cla4 block15 (.a(a[63:60]), .b(b[63:60]), .cin(c[15]), .sum(sum[63:60]), .cout(cout));
   // TODO: your sixteen cla4 instances go here.
 
-endmodule‹
+endmodule
